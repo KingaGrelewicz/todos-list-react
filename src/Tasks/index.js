01c4b41567ad35cc9e6fdc1b5a5e0380cf199c58
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-09-10 18:10:19 +0200

    destructuring added to Tasks function

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,23 +1,24 @@
 import "./style.css";
 
-const Tasks = (props) => (
+const Tasks = ({ tasks, hideDoneTasks }) => (
     <ul className="tasks">
-        {props.tasks.map(task => (
-            <li key={task.id}
-                className={`list__item${task.done && props.hideDoneTasks ? " list__item--hidden" : ""}`}
-            >
-                <button className="list__button list__button--toggleDone">
-                    {task.done ? "✔" : ""}
-                </button>
-                <span className={`"list__content${task.done ? " list__content--done" : ""}"`}>
-                {task.content}
-                </span>
-                <button className="list__button list__button--remove">
-                    🗑
-                </button>
-            </li>
-        ))}
-    </ul >
-);
+      {tasks.map(task => (
+        <li
+          key={task.id}
+          className={`list__item${task.done && hideDoneTasks ? " list__item--hidden" : ""}`}
+        >
+          <button className="list__button list__button--toggleDone">
+            {task.done ? "✔" : ""}
+          </button>
+          <span className={`list__content${task.done ? " list__content--done" : ""}`}>
+            {task.content}
+          </span>
+          <button className="list__button list__button--remove">
+            🗑
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 
 export default Tasks;
