a44883ea6c2594c39da8d8725fa9c6b8ea3f6416
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-09-14 19:52:58 +0200

    removeTask interaction added

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,26 +1,27 @@
 import "./style.css";
 
-const Tasks = ({ tasks, hideDone }) => (
-    <ul className="tasks">
-      {tasks.map(task => (
-        <li
-          key={task.id}
-          className={`list__item${task.done && hideDone ? " list__item--hidden" : ""}`}
-        >
-          <button className="list__button list__button--toggleDone">
-            {task.done ? "✔" : ""}
-          </button>
-          <span className={`list__content${task.done ? " list__content--done" : ""}`}>
-            {task.content}
-          </span>
-          <button 
+const Tasks = ({ tasks, hideDone, removeTask }) => (
+  <ul className="tasks">
+    {tasks.map(task => (
+      <li
+        key={task.id}
+        className={`list__item${task.done && hideDone ? " list__item--hidden" : ""}`}
+      >
+        <button className="list__button list__button--toggleDone">
+          {task.done ? "✔" : ""}
+        </button>
+        <span className={`list__content${task.done ? " list__content--done" : ""}`}>
+          {task.content}
+        </span>
+        <button
+          onClick={() => removeTask(task.id)}
           className="list__button list__button--remove"
-          >
-            🗑
-          </button>
-        </li>
-      ))}
-    </ul>
-  );
+        >
+          🗑
+        </button>
+      </li>
+    ))}
+  </ul>
+);
 
 export default Tasks;
