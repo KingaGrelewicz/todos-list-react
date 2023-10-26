eb34dbf32c3ea3f7f36d447a5185ebd5159f4174
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-10-26 18:00:00 +0200

    buttons style moved to styled using styled-components

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,22 +1,21 @@
-import "./style.css";
+import { SectionButtons } from "./styled"
 
 const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-  <div className="buttons__buttons">
+  <SectionButtons>
     {tasks.length > 0 && (
       <>
-        <button onClick={toggleHideDone} className="buttons__button">
+        <SectionButtons onClick={toggleHideDone}>
           {hideDone ? "Pokaż" : "Ukryj"} ukończone
-        </button>
-        <button
+        </SectionButtons>
+        <SectionButtons
           onClick={setAllDone}
-          className="buttons__button"
           disabled={tasks.every(({ done }) => done)}
         >
           Ukończ wszystkie
-        </button>
+        </SectionButtons>
       </>
     )}
-  </div>
+  </SectionButtons>
 );
 
 export default Buttons;
---
+++ src/Buttons/styled.js
@@ -0,0 +1,36 @@
+import styled, { css } from "styled-components"
+
+export const SectionButtons = styled.div`
+    background-color: white;
+    display: flex;
+    border: none;
+    color: teal;
+    padding: 20px 15px;
+    cursor: pointer;
+    transition: 0.5s;
+
+        &:hover {
+            color: hsl(180, 96%, 35%);
+        }
+
+        &:active{
+            color: hsl(180, 96%, 40%);
+        }
+
+        ${({ disabled }) => disabled && css`
+            color: gray;
+            cursor: not-allowed;
+            &:hover {
+                color: gray
+            }
+        `}
+    
+        @media (max-width: 768px) {
+            display: grid;
+            grid-template-columns: 1fr;
+            justify-content: center;
+            border: none;
+            padding: 10px;
+            margin: 0 auto;
+        }
+`;
