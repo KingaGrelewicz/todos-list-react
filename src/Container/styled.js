00415e7a10df02a98b9823baaef413aba4c2e499
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-10-26 18:00:12 +0200

    container style moved to styled using styled-components

---
+++ src/Container/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const StyledContainer = styled.main`
+    max-width: 800px;
+    padding: 20px;
+    margin: 0 auto;
+  
+    @media (max-width: 768px) {
+        margin: 0 15px;
+    }
+`;
