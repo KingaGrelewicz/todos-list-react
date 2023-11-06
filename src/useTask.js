fcdcf24654fbd212ec2077ec6ebd0ebbbe680d72
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-11-06 11:43:27 +0100

    if changed to ternary

--- src/useTask.js
+++ src/useTask.js
@@ -6,18 +6,16 @@ export const useTask = () => {
     useEffect(() => {
         localStorage.setItem("tasks", JSON.stringify(tasks));
     }, [tasks]);
+    
     const removeTask = (id) => {
         setTasks(tasks => tasks.filter(task => task.id !== id));
     };
 
     const toggleTaskDone = (id) => {
-        setTasks(tasks => tasks.map(task => {
-            if (task.id === id) {
-                return { ...task, done: !task.done };
-            }
-
-            return task;
-        }))
+        setTasks(tasks => tasks.map(task =>
+            (task.id === id ?
+               { ...task, done: !task.done } : task))
+        )
     };
 
     const setAllDone = () => {
