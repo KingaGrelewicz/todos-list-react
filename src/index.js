0e355ecdfc1414ffb882406de06a5c436d1e9bf8
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-12-16 20:27:26 +0100

    aplication split info features

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { App } from "./App";
 import reportWebVitals from './reportWebVitals';
+// import { configureStore } from "@reduxjs/toolkit";
 import { ThemeProvider } from 'styled-components';
 import { theme } from "./theme";
+
+// const initialState = {
+//   tasks: [],
+// };
+
+// const taskReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "addTask":
+//       return {
+//         ...state,
+//         tasks: [
+//           ...state.tasks,
+//           {
+//             content: action.payload
+//           },
+//         ],
+//       };
+
+//     default:
+//       return state;
+//   }
+// };
+
+// const addTask = content => ({
+//   type: "addTask",
+//   payload: content,
+// })
+
+// const store = configureStore({reducer: taskReducer});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <App />
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
