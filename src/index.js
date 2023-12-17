e1b1aa2aba85c9b8106d6ff2b9cc8e106f556048
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-12-17 19:48:31 +0100

    Provider imported

--- src/index.js
+++ src/index.js
@@ -2,45 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from "./App";
 import reportWebVitals from './reportWebVitals';
-// import { configureStore } from "@reduxjs/toolkit";
 import { ThemeProvider } from 'styled-components';
+import { Provider } from "react-redux";
 import { theme } from "./theme";
-
-// const initialState = {
-//   tasks: [],
-// };
-
-// const taskReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case "addTask":
-//       return {
-//         ...state,
-//         tasks: [
-//           ...state.tasks,
-//           {
-//             content: action.payload
-//           },
-//         ],
-//       };
-
-//     default:
-//       return state;
-//   }
-// };
-
-// const addTask = content => ({
-//   type: "addTask",
-//   payload: content,
-// })
-
-// const store = configureStore({reducer: taskReducer});
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
