477411d9436e2970713beab39dcbf715ed3b9cdb
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-12-31 15:05:42 +0100

    tasksSaga created

---
+++ src/features/tasks/tasksSaga.js
@@ -0,0 +1,24 @@
+import { takeLatest, call, put, delay, select, takeEvery } from "redux-saga/effects";
+import { fetchExampleTasks, selectTasks, setTasks } from "./taskSlice";
+import { getExampleTasks } from "./getExampleTasks";
+import { saveTasksInLocalStorage } from "./taskLocalStorage";
+
+function* fetchExampleTaskHandler() {
+    try {
+        yield delay(1000); 
+        const exampleTasks = yield call(getExampleTasks)
+        yield put(setTasks(exampleTasks))
+    } catch (error) {
+        yield put(console.error("ups, coś poszło nie tak"));
+    }
+}
+
+function* saveTasksInLocalStorageHandler() {
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocalStorage, tasks);
+}
+
+export function* watchFetchExampleTasks() {
+    yield takeLatest(fetchExampleTasks.type, fetchExampleTaskHandler);
+    yield takeEvery("*", saveTasksInLocalStorageHandler);
+}
