e03bc4629b6e4ded750a4f0f31b9e6dfe6c39a16
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2023-12-31 15:04:56 +0100

    exampleTasks button and API function added

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import { Container, Button } from "./styled"
-import { selectTasks, toggleHideDone, setAllDone } from "../taskSlice";
+import { selectTasks, toggleHideDone, setAllDone, fetchExampleTasks } from "../taskSlice";
 
 const Buttons = () => {
   const { tasks, hideDone } = useSelector(selectTasks);
@@ -8,6 +8,9 @@ const Buttons = () => {
 
   return (
     <Container>
+      <Button onClick={() => dispatch(fetchExampleTasks())}>
+            Pobierz przykładowe zadania
+        </Button>
       {tasks.length > 0 && (
         <>
           <Button onClick={() => dispatch(toggleHideDone())}>
