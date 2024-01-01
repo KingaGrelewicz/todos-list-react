24de5227437b93cdc1d56df5fd79eca97e38bef6
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-01-02 00:23:52 +0100

    AuthorPage.js created

---
+++ src/features/author/AuthorPage.js
@@ -0,0 +1,22 @@
+import Container from "../../common/Container"
+import Header from "../../common/Header"
+import Section from "../../common/Section"
+
+const AuthorPage = () => (
+    <Container>
+        <Header title="O autorze" />
+        <Section
+            title="Kinga Grelewicz"
+            body={
+                <>
+                    <p>
+                        W czasie wolnym lubię zatrzymać się na chwilę przy kawie oraz lub szydełkując nowe projekty.
+                        Bardzo lubię podróżować oraz poznawać inne kultury, spacerować pośród nowych nieznanych mi jeszcze miejsc.
+                    </p>
+                </>
+            }
+        />
+    </Container>
+);
+
+export default AuthorPage;
