5e1b28edf599f835a04fe0838650ea765a70ce98
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-01-02 20:00:52 +0100

    Input styled updated

--- src/features/tasks/Input/index.js
+++ src/features/tasks/Input/index.js
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export default styled.input`
     padding: 10px;
     border: 1px solid ${({ theme }) => theme.colors.westar};
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+        width: 100%;
+    }
 `;
