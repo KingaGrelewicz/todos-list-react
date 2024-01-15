9968dbaa952793f8c3690652da4d37dd70ddee0d
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-01-15 13:00:51 +0100

    author page and styled.js added

--- src/common/Section/index.js
+++ src/common/Section/index.js
@@ -1,15 +1,20 @@
 import { Wrapper, Title, SectionList } from "./styled"
 
-const Section = ({ title, body, extraHeaderContent }) => (
-  <Wrapper>
-    <SectionList>
-      <Title>{title}</Title>
-      {extraHeaderContent}
-    </SectionList>
-    <div>
-      {body}
-    </div>
-  </Wrapper>
-);
+const Section = ({ title, body, extraHeaderContent }) => {
+
+  return (
+    <Wrapper>
+      <SectionList>
+        <Title>
+          {title}
+        </Title>
+        {extraHeaderContent}
+      </SectionList>
+      <div>
+        {body}
+      </div>
+    </Wrapper>
+  )
+};
 
 export default Section;
--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -1,6 +1,7 @@
 import Container from "../../common/Container"
 import Header from "../../common/Header"
 import Section from "../../common/Section"
+import { Wrapper } from "./styled";
 
 const AuthorPage = () => (
     <Container>
@@ -8,12 +9,13 @@ const AuthorPage = () => (
         <Section
             title="Kinga Grelewicz"
             body={
-                <>
+                <Wrapper>
                     <p>
+                        Na codzień pracuję jako koordynator do spraw szkoleń w Chińskiej korporacji.
                         W czasie wolnym lubię zatrzymać się na chwilę przy kawie oraz serialu lub szydełkując nowe projekty.
                         Bardzo lubię podróżować oraz poznawać inne kultury, spacerować pośród nowych nieznanych mi jeszcze miejsc.
                     </p>
-                </>
+                </Wrapper>
             }
         />
     </Container>
