9b76393dd7b96cc2b509ed13db6b6d7a006d5724
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-01-15 12:50:48 +0100

    styled.js updated

--- src/features/tasks/TasksPage/TaskList/styled.js
+++ src/features/tasks/TasksPage/TaskList/styled.js
@@ -1,4 +1,6 @@
-import styled, { css } from "styled-components"
+import styled, { css } from "styled-components";
+import { Link } from "react-router-dom";
+
 
 export const List = styled.ul`
     margin: 0 auto;
@@ -20,12 +22,13 @@ export const Item = styled.li`
 `;
 
 export const Content = styled.span`
+    text-decoration: none;
     ${({ $done }) => $done && css`
         text-decoration: line-through;
     `}
 `;
 
-export const Button = styled.button`
+export const StyledButton = styled.button`
     border: none;
     color: ${({ theme }) => theme.colors.white};
     width: 30px;
@@ -58,4 +61,20 @@ export const Button = styled.button`
             filter: brightness(120%);
         }
     `}
+`;
+
+export const StyledLink = styled(Link)`
+    text-decoration: none;
+
+    &:hover {
+            filter: brightness(110%);
+        }
+
+    &:active {
+        filter: brightness(120%);
+    }
+
+    &:visited {
+        color: ${({ theme }) => theme.colors.teal};
+    }
 `;
