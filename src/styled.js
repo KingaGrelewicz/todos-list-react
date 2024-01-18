65e731220e18c4a5fd25924e36eb2d8b2b39931f
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-01-18 13:01:22 +0100

    transition removed

--- src/styled.js
+++ src/styled.js
@@ -12,12 +12,7 @@ export const StyledNav = styled.ul`
 export const StyledNavLink = styled(NavLink)`
     text-decoration: none;
     color: ${({ theme }) => theme.colors.white};
-    transition: 0.5s;
-
-    &:hover {
-        transform: scale (1.1);
-    }
-
+    
     &:active {
         font-weight: bold;
     }
