e4d620fbaab6b45d44477282e204f4b03e403252
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-01-24 10:55:25 +0100

    transform added

--- src/styled.js
+++ src/styled.js
@@ -12,7 +12,7 @@ export const StyledNav = styled.ul`
 export const StyledNavLink = styled(NavLink)`
     text-decoration: none;
     color: ${({ theme }) => theme.colors.white};
-    transition: 1s;
+    transition: 0.5s;
 
     &:hover {
         transform: scale(1.1);
