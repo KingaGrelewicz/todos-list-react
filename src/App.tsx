354e78b418c12136906c597546b248e16c73d1c4
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-08-16 12:47:54 +0200

    App.js conveted to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import { Switch, Route, HashRouter, Redirect } from "react-router-dom";
 import TasksPage from "./features/tasks/TasksPage";
 import { TaskPage } from "./features/tasks/TaskPage";
-import AuthorPage from "./features/author/AuthorPage";
-import { StyledNavLink, StyledList, StyledNav } from "./styled";
+import AuthorPage from "./features/author/index";
+import { StyledNavLink, StyledNav, StyledList } from "./styled";
 
-
-export const App = () => (
+export const App: React.FC = () => (
   <HashRouter>
     <nav>
       <StyledNav>
         <StyledList>
-          <StyledNavLink exact to="/zadania">Zadania</StyledNavLink>
+          <StyledNavLink exact to="/zadania">
+            Zadania
+          </StyledNavLink>
         </StyledList>
         <StyledList>
-          <StyledNavLink exact to="/autor">O autorze</StyledNavLink>
+          <StyledNavLink exact to="/autor">
+            O autorze
+          </StyledNavLink>
         </StyledList>
       </StyledNav>
       <Switch>
