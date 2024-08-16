c1921e4cd578e7ff7188a6fe0efed0e30a1bb935
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-08-16 12:50:29 +0200

    useTask.js deleted

--- src/useTask.js
+++
@@ -1,9 +0,0 @@
-// import { useState, useEffect } from 'react';
-
-// export const useTask = () => {
-//     const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
-
-//     useEffect(() => {
-//         localStorage.setItem("tasks", JSON.stringify(tasks));
-//     }, [tasks]);
-// }
